cfabd997c306876986010a2d6974342cd58e63ed
Andy Freeland <andy@andyfreeland.net>
2011-05-26 12:00:46 -0500

    --amend

--- futures_market/market/models.py
+++ futures_market/market/models.py
@@ -6,8 +6,9 @@ class Market(models.Model):
     creation_date = models.DateTimeField('date created')
     stock_endowment = models.IntegerField() # initial stock endowment for users
     cash_endowment = models.IntegerField() # initial cash endowment for users
-    market_open = models.BooleanField()
     total_shares = models.IntegerField() # number_users * endowment
+    
+    market_open = models.BooleanField()
 
 class Stock(models.Model):
     market = models.ForeignKey(Market)
@@ -15,31 +16,37 @@ class Stock(models.Model):
     description = models.TextField()
     # max liquidation_price $999999.99. this may be larger than necessary
     liquidation_price = models.DecimalField(max_digits=8, decimal_places=2)
+    
     last_sale_price = models.DecimalField(max_digits=8, decimal_places=2)
     last_sale_time = models.DateTimeField('last sale')
 
 class Trader(models.Model):
     market = models.ForeignKey(Market)
     name = models.CharField(max_length=20) # no particular reason
+    
     cash = models.DecimalField(max_digits=8, decimal_places=2) # max_digits should be larger?
 
 class Holding(models.Model):
     market = models.ForeignKey(Market)
     stock = models.ForeignKey(Stock)
     trader = models.ForeignKey(Trader)
+    
     shares = models.IntegerField()
 
 class Order(models.Model):
     market = models.ForeignKey(Market)
     stock = models.ForeignKey(Stock)
     trader = models.ForeignKey(Trader)
+    
     ORDER_CHOICES = (
             ('B', 'Buy'),
             ('S', 'Sell'),
         )
     order = models.CharField(max_length=1, choices=ORDER_CHOICES)
-    completed = models.BooleanField()
-    completion_time = models.DateTimeField()
     creation_time = models.DateTimeField()
     volume = models.IntegerField()
     price = models.DecimalField(max_digits=8, decimal_places=2)
+    
+    completed = models.BooleanField()
+    completion_time = models.DateTimeField()
+    
