06f962f968ed10c22a867e7671367247e5432d94
Andy Freeland <andy@andyfreeland.net>
2011-05-26 16:11:20 -0500

    adding features to admin

--- futures_market/market/admin.py
+++ futures_market/market/admin.py
@@ -1,4 +1,12 @@
-from market.models import Market
+from market.models import *
 from django.contrib import admin
 
-admin.site.register(Market)
+class MarketAdmin(admin.ModelAdmin):
+    fields = ['question', 'description', 'cash_endowment', 'market_open']
+    readonly_fields = ['creation_date']
+
+admin.site.register(Market, MarketAdmin)
+admin.site.register(Stock)
+admin.site.register(Trader)
+admin.site.register(Holding)
+admin.site.register(Order)
--- futures_market/market/models.py
+++ futures_market/market/models.py
@@ -4,7 +4,7 @@ from datetime import datetime
 class Market(models.Model):
     question = models.CharField(max_length=500)
     description = models.TextField(blank=True)
-    creation_date = models.DateTimeField('date created',default=datetime.now(),editable=False)
+    creation_date = models.DateTimeField('date created',default=datetime.now())
     cash_endowment = models.DecimalField(max_digits=8, decimal_places=2) # initial cash endowment for users, DLN is not sure what makes sense with this.
     # might be easier to make "cash" into a particular stock, with known fixed liquidation price == 1??
     market_open = models.BooleanField()
