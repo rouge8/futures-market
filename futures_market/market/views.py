88da1ccd7108d1eaadd7c61796b572d8a6ea05fa
Andy Freeland <andy@andyfreeland.net>
2011-06-16 15:24:38 -0500

    initial order placing

--- futures_market/market/views.py
+++ futures_market/market/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
+from django.http import HttpResponseRedirect
+from market.forms import *
 from market.models import *
 
 def index(request):
@@ -10,5 +13,25 @@ def market(request, market_slug):
 
 def trader(request, market_slug, trader_name):
     m = get_object_or_404(Market, slug=market_slug)
-    t = get_object_or_404(Trader, name=trader_name)
-    return render_to_response('market/trader.html', {'trader': t})
+    t = get_object_or_404(Trader, name=trader_name, market=m)
+    
+    if request.method == 'POST': # Order being placed!
+        form = OrderForm(request.POST)
+
+        if form.is_valid():
+            order = form.save(commit = False)
+            order.market = m
+            order.trader = t
+            order.save()
+
+            return HttpResponseRedirect(request.path)
+
+    else: # normal GET request
+        if t:
+            open_orders = Order.objects.filter(market=m, trader=t, completed=False)
+            closed_orders = Order.objects.filter(market=m, trader=t, completed=True)
+            form = OrderForm()
+
+            data = {'trader': t, 'open_orders': open_orders, 'closed_orders': closed_orders, 'form': form} 
+
+        return render_to_response('market/trader.html', data, context_instance=RequestContext(request))
