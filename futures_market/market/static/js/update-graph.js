c3a4939811a40be27afc0b2ef550b78aa17dbdda
Andy Freeland <andy@andyfreeland.net>
2011-06-24 02:03:47 -0500

    removed unnecessary function and initial 1 second delay from updateGraph

--- futures_market/market/static/js/update-graph.js
+++ futures_market/market/static/js/update-graph.js
@@ -9,30 +9,26 @@ function updateGraph(market) {
 
     //$.plot(placeholder, data, options);
 
-    function fetchData() {
-        function onDataReceived(stocks) {
-            // plot each stock
-            // we get all the data in one go, if we only got partial data
-            // we could merge it with what we already have
-            data = [];
+    function onDataReceived(stocks) {
+        // plot each stock
+        // we get all the data in one go, if we only got partial data
+        // we could merge it with what we already have
+        data = [];
 
-            $.each(stocks, function(index, stock) {
-                // add to current data
+        $.each(stocks, function(index, stock) {
+            // add to current data
 
-                data.push(stock);
+            data.push(stock);
 
-                // plot!
-                $.plot(placeholder, data, options);
-            });
-        }
-
-        $.ajax({
-            url: '/orders/' + market + '/',
-            method: 'GET',
-            dataType: 'json',
-            success: onDataReceived
+            // plot!
+            $.plot(placeholder, data, options);
         });
     }
 
-    setTimeout(fetchData, 1000);
+    $.ajax({
+        url: '/orders/' + market + '/',
+        method: 'GET',
+        dataType: 'json',
+        success: onDataReceived
+    });
 };
