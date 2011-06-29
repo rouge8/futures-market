970fbbb826678d02890113a8a9956090f621f525
Andy Freeland <andy@andyfreeland.net>
2011-06-29 11:23:54 -0500

    documented update-prices.js

--- futures_market/market/static/js/update-prices.js
+++ futures_market/market/static/js/update-prices.js
@@ -1,7 +1,9 @@
 function updatePrices(market) {
+    // gets latest prices as JSON data
     $.getJSON('/prices/' + market + '/', function(data) {
         var stocks = [];
 
+        // iterates over stocks and assembles them into nicely formatted lists
         $.each(data, function(index, stock) {
             stocks.push('<li id="' + stock.name + '">' + stock.name +
                 "'s last price: " + stock.last_sale_price + ' at ' +
@@ -9,6 +11,8 @@ function updatePrices(market) {
                 '; best sell offer: ' + stock.best_sale + ')</li>');
         });
 
+        // joins all the stock list into an unordered list and replaces the
+        // previous one
         $('<ul/>', {
             'id': 'stock-list',
             html: stocks.join('')
