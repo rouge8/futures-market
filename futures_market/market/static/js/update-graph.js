fe1a60d21d35e05ee6afe88b56c1d81d83168e9a
Andy Freeland <andy@andyfreeland.net>
2011-07-11 11:31:26 -0500

    current time is now added in graph javascript so that view can be cached

--- futures_market/market/static/js/update-graph.js
+++ futures_market/market/static/js/update-graph.js
@@ -16,6 +16,21 @@ function updateGraph(url) {
         $.each(stocks, function(index, stock) {
             // add to current data
 
+            if (stock.data.length > 0) {
+                var last_price = stock.data[stock.data.length-1][1];
+                d = new Date();
+                utc = d.getTime();
+                //localOffset = d.getTimezoneOffset() * 60000;
+                //utc = localTime + localOffset;
+                offset = -5;
+                var now = utc + (3600000*offset);
+
+                var current = new Array(now, last_price);
+                stock.data.push(current);
+                //console.log(now);
+                //console.log(last_price);
+                console.log(stock);
+            }
             data.push(stock);
 
             // plot!
