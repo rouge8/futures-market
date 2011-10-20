87d174449dc5e999ce4c26a2f6a246661e1d59a0
Andy Freeland <andy@andyfreeland.net>
2011-10-20 13:16:04 -0500

    prices can now go up to 2.00

--- futures_market/market/forms.py
+++ futures_market/market/forms.py
@@ -6,8 +6,8 @@ class OrderForm(ModelForm):
     # from http://stackoverflow.com/questions/1697702/how-to-pass-initial-parameter-to-djangos-modelform-instance/1697770#1697770
     # price from http://stackoverflow.com/questions/6473895/how-to-restrict-values-in-a-django-decimalfield
 
-    # restricts prices to 0.0 through 1.0
-    PRICE_CHOICES = [(i*.01, str(i*.01)) for i in range(1,101)]
+    # restricts prices to 0.0 through 2.0
+    PRICE_CHOICES = [(i*.01, str(i*.01)) for i in range(1,201)]
     price = ChoiceField(choices=PRICE_CHOICES)
     trader = ModelChoiceField(label='', queryset=Trader.objects.all(), widget=HiddenInput())
     market = ModelChoiceField(label='', queryset=Market.objects.all(), widget=HiddenInput())
